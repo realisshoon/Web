b389174152634ed3011bce7c3fefe27b3518b151
iamseungman <hsgn21@naver.com>
2022-08-15 19:44:52 +0900

    Web Framework의 Create

--- DjangoStudy/myapp/views.py
+++ DjangoStudy/myapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render,HttpResponse,redirect
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+nextId=4
 topics=[
 {'id':1, 'title':'routing', 'body':'Routing is..'},
 {'id':2, 'title':'view', 'body':'view is..'},
@@ -43,12 +45,23 @@ def read(request,id):
             article=f'<h2>{topic["title"]}</h2>{topic["body"]}'
     return HttpResponse(HTMLTemplate(article))
 
+@csrf_exempt #csrf 면제
 def create(request):
-    article='''
-        <form action="/create/"post>
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p><textarea name="body" placeholder="body"></textarea></p>   
-            <p><input type="submit"></p>
-        </form>
-    '''
-    return HttpResponse(HTMLTemplate(article))
+    global nextId
+    if request.method =='GET':
+        article='''
+            <form action="/create/"method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>   
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method== 'POST':
+        title=request.POST['title']
+        body=request.POST['body']
+        newTopic={"id":nextId,"title":title,"body":body}
+        topics.append(newTopic)
+        url='/read/'+str(nextId)
+        nextId=nextId+1
+        return redirect(url)
