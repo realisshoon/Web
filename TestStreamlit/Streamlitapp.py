ea2bc5cc6c05466703d64295c004c657a38a49be
iamseungman <hsgn21@naver.com>
2022-08-23 18:27:51 +0900

    <app> pybo: python 질의응답 게시판 <template> <model>

---
+++ TestStreamlit/Streamlitapp.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Ausra')
+
+tab1,tab2,tab3= st.tabs(["intro","pick sound","community"])
+
+
+with tab1:
+     st.header("Welcome we are Asura")
+
+with tab2:
+     st.header("Choose the sound you want to detect")
+     ##소리 선택
+with tab3:
+     st.write("질문을 등록해주세요")
+
